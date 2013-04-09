504b37f8418fd68a687562ea7ced8a88870e31df
wgh <appwgh@gmail.com>
2013-04-09 10:42:46 +0800

    first commit

---
+++ SingleSelectFlagPicker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  SingleSelectFlagPicker
+//
+//  Created by wangguohui on 13-4-9.
+//  Copyright (c) 2013年 TuShengNetWork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ SingleSelectFlagPicker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  SingleSelectFlagPicker
+//
+//  Created by wangguohui on 13-4-9.
+//  Copyright (c) 2013年 TuShengNetWork. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
